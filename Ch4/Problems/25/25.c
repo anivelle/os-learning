261d8a3344eb1613426585f94409512b0b4c2524
Alex Nivelle <anivelle@ucsc.edu>
2023-12-22 01:15:24 -0800

    Yeah still don't know what's up with the OpenMP making it slower. May have to do with generating a bunch of random numbers, or I initialized OpenMP improperly (which, just going by the book, doesn't seem to be the case, though the book did *not* explain the necessary compiler flags

--- Ch4/Problems/25/25.c
+++ Ch4/Problems/25/25.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <omp.h>
 
 #define NUM_POINTS 100000000
 
+struct point {
+    float x;
+    float y;
+};
+
 int main(int argc, char *argv[]) {
 
-  srand(time(NULL));
-  int i;
-  int points_in = 0;
+    srandom(time(NULL));
+    int i;
+    int points_in = 0;
 
-  // Parallelized point generation
-  #pragma omp parallel for
-  for (i = 0; i < NUM_POINTS; i++){
-    float x = (float)rand() / RAND_MAX * 2 - 1;
-    float y = (float)rand() / RAND_MAX * 2 - 1;
-    if (sqrt(pow(x, 2) + pow(y, 2)) < 1) 
-      points_in++;
-  }
+    float x, y;
+// Parallelized point generation
+#pragma omp parallel for schedule(static)
+    for (i = 0; i < NUM_POINTS; i++) {
+        x = (float)random() / RAND_MAX * 2 - 1;
+        y = (float)random() / RAND_MAX * 2 - 1;
+        if (sqrt(pow(x, 2) + pow(y, 2)) < 1)
+            points_in++;
+    }
 
-  float pi = 4 * (float)points_in / NUM_POINTS;
-  printf("Pi is: %f\n", pi);
+    float pi = 4 * (float)points_in / NUM_POINTS;
+    printf("Pi is: %f\n", pi);
 
-  return 0;
+    return 0;
 }
